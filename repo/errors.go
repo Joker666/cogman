882d75b3e87e0119a1e7715a3976e1d6c88ad272
Chowdhury osman <chowdhuryosman04@gmail.com>
2019-07-29 18:54:08 +0600

    check mongo connection nil check in every iteration after sleep

--- repo/errors.go
+++ repo/errors.go
@@ -3,7 +3,9 @@ package repo
 import "errors"
 
 var (
-	ErrTaskNotFound     = errors.New("mongo: task not found")
-	ErrErrorRequired    = errors.New("error message required")
-	ErrDurationRequired = errors.New("task duration required")
+	ErrTaskNotFound      = errors.New("mongo: task not found")
+	ErrMongoNoConnection = errors.New("mongo: no connection")
+	ErrRedisNoConnection = errors.New("redis: no connection")
+	ErrErrorRequired     = errors.New("error message required")
+	ErrDurationRequired  = errors.New("task duration required")
 )
