7276f76e74d82be3c817bd3621de9d4b23475993
Chowdhury osman <chowdhuryosman04@gmail.com>
2019-07-30 15:13:16 +0600

    Sending error if queue not available

--- client/queue.go
+++ client/queue.go
@@ -8,18 +8,21 @@ import (
 	"github.com/streadway/amqp"
 )
 
-func (s *Session) formQueueName(taskType util.TaskPriority) string {
+func (s *Session) formQueueName(taskType util.TaskPriority) (string, error) {
 	queueType := util.LowPriorityQueue
 	if taskType == util.TaskPriorityHigh {
 		queueType = util.HighPriorityQueue
 	}
 
-	id := s.getQueueIndex(taskType)
+	id, err := s.getQueueIndex(taskType)
+	if err != nil {
+		return "", err
+	}
 
-	return queueType + "_" + strconv.Itoa(id)
+	return queueType + "_" + strconv.Itoa(id), nil
 }
 
-func (s *Session) getQueueIndex(taskType util.TaskPriority) int {
+func (s *Session) getQueueIndex(taskType util.TaskPriority) (int, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
@@ -33,23 +36,30 @@ func (s *Session) getQueueIndex(taskType util.TaskPriority) int {
 		mode = util.QueueModeDefault
 	}
 
+	if count[mode] == 0 {
+		return 0, ErrQueueNotAvailable
+	}
+
 	index := s.queueIndex[mode]
 	s.queueIndex[mode] = (s.queueIndex[mode] + 1) % count[mode]
 
-	return index
+	return index, nil
 }
 
-func (s *Session) GetQueueName(pType util.TaskPriority) string {
+func (s *Session) GetQueueName(pType util.TaskPriority) (string, error) {
 	name := ""
 	for {
-		queue := s.formQueueName(pType)
+		queue, err := s.formQueueName(pType)
+		if err != nil {
+			return "", err
+		}
 		if _, err := s.EnsureQueue(s.conn, queue, pType); err == nil {
 			name = queue
 			break
 		}
 	}
 
-	return name
+	return name, nil
 }
 
 func (s *Session) EnsureQueue(con *amqp.Connection, queue string, taskType util.TaskPriority) (*amqp.Queue, error) {
