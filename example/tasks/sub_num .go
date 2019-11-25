2aa323751d07384072e00f5234b6e713b1eca383
HR <mr.k779@outlook.com>
2019-11-26 01:05:59 +0600

    Updated example

--- example/tasks/sub_num .go
+++ example/tasks/sub_num .go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/Tapfury/cogman/util"
 )
@@ -24,6 +25,8 @@ func (t SubTask) Do(ctx context.Context, payload []byte) error {
 		log.Print("Subtraction task process error", err)
 		return err
 	}
+
+	time.Sleep(time.Millisecond * 300)
 
 	log.Printf("num1: %d num2: %d sub: %d", body.Num1, body.Num2, body.Num1-body.Num2)
 	return nil
--- example/tasks/sum_num.go
+++ example/tasks/sum_num.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/Tapfury/cogman/util"
 )
 
 type SumTask struct {
-	Name string 
+	Name string
 }
 
 func NewSumTask() util.Handler {
@@ -24,6 +25,8 @@ func (t SumTask) Do(ctx context.Context, payload []byte) error {
 		log.Print("Sum task process error", err)
 		return err
 	}
+
+	time.Sleep(time.Millisecond * 300)
 
 	log.Printf("num1: %d num2: %d sum: %d", body.Num1, body.Num2, body.Num1+body.Num2)
 	return nil
